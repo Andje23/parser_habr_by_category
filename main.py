618a57af0798a435b491ef13c6476fcd151cb6df
Andje23 <andje23@gmail.com>
2023-01-10 22:40:26 +0300

    Getting link to articles.

--- main.py
+++ main.py
@@ -14,9 +14,10 @@ with open("proxy.txt") as file:
 
 async def send_request(url: str, rand_proxy: str) -> str:
     async with aiohttp.ClientSession() as session:
+        async with session.get(url, proxy=f"http://{rand_proxy}") as resp:
             return await resp.text(encoding="utf-8")
 
-        async with session.get(url, proxy=f"http://{rand_proxy}") as resp:
+
 
 async def parse_category(category_url: str):
     random_proxy = random.choice(PROXY_LIST)
@@ -24,6 +25,22 @@ async def parse_category(category_url: str):
     soup = BeautifulSoup(html_response, "lxml")
     pagination_block = soup.find("div", class_="tm-pagination__pages")
     pages_count = pagination_block.find_all("a", class_="tm-pagination__page")[-1].text.strip()
+
+    for page in range(int(pages_count)):
+        page_response = await send_request(
+            url=f"{category_url}/page{page}/",
+            rand_proxy=random_proxy
+        )
+
+        page_soup = BeautifulSoup(page_response, "lxml")
+        articles = page_soup.find_all("article", class_="tm-articles-list__item")
+
+        for article in articles:
+            info_block = article.find("a", class_="tm-article-snippet__title-link")
+            title = info_block.find("span").text.strip()
+            link = f"https://{info_block.get('href')}"
+
+            print(link)
 
 
 
